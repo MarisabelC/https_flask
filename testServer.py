8bf4cbda371392dc9883a246bf54a25c1be5c13a
MarisabelC <38087342+MarisabelC@users.noreply.github.com>
2021-10-28 08:56:26 -0700

    Create testServer.py

---
+++ testServer.py
@@ -0,0 +1,20 @@
+from flask import Flask, jsonify
+import os
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def index():
+    return 'Flask is running!'
+
+
+@app.route('/data')
+def names():
+    data = {"names": ["John", "Jacob", "Julie", "Jennifer"]}
+
+    return jsonify(data)
+
+
+if __name__ == '__main__':
+    app.run(host = '0.0.0.0')
